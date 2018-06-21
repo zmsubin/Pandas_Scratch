d10ae3c5867b1403eeb8e3634eb0280a9f0957f2
zmsubin <subin@berkeley.edu>
2018-06-21 15:45:23 -0700

    Change ylim

--- High_Electrification_Waterfall.py
+++ High_Electrification_Waterfall.py
@@ -1,13 +1,4 @@
-
-# coding: utf-8
-
-# In[15]:
-
-
 import waterfall_chart
-
-#import ethree_col
-
 
 measure_names = ['Reference', 'Renewables', 'LDV\nElectrification', 'Heat\nPumps', 'Non-\nCombustion', 'Other']
 measure_quantities = [433, -103, -57, -27, -51, 86.2-433+(103+57+27+51)]
@@ -15,8 +6,6 @@ plot = waterfall_chart.plot(measure_names, measure_quantities, rotation_value=0,
                             net_label='2050 Goal', Title='CA GHG Emissions in 2050', y_lab = r'MMT CO$_2$e',
                             green_color = 'red', red_color = 'green', formatting='{:,.0f}')
 
+plot.ylim([0, 500])
+
 plot.savefig('waterfall.pdf')
-
-# In[2]:
-
-
