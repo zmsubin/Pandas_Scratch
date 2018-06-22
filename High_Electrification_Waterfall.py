89a8b2e3446a035fc93c31167837791913adb45b
zmsubin <subin@berkeley.edu>
2018-06-21 17:41:48 -0700

    Collapse 2 categories

--- High_Electrification_Waterfall.py
+++ High_Electrification_Waterfall.py
@@ -1,7 +1,7 @@
 import waterfall_chart
 
-measure_names = ['Reference', 'Renewables', 'LDV\nElectrification', 'Heat\nPumps', 'Non-\nCombustion', 'Other']
-measure_quantities = [433, -103, -57, -27, -51, 86.2-433+(103+57+27+51)]
+measure_names = ['Reference', 'Renewables', 'LDV\nElectrification', 'Heat\nPumps', 'Other GHG\nReductions']
+measure_quantities = [433, -103, -57, -27, 86.2-433+(103+57+27)]
 plot = waterfall_chart.plot(measure_names, measure_quantities, rotation_value=0, figsize=(7, 4),
                             net_label='2050 Goal', Title='CA GHG Emissions in 2050', y_lab = r'MMT CO$_2$e',
                             green_color = 'red', red_color = 'green', formatting='{:,.0f}')
