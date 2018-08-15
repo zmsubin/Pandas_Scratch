a13e8fd383fcd623f9cbd8c8e4c800aaed0fb96c
zmsubin <subin@berkeley.edu>
2018-08-15 11:04:02 -0700

    Update bldg energy

--- Bldgs/Bldg_Energy.py
+++ Bldgs/Bldg_Energy.py
@@ -18,34 +18,45 @@ kbtu_to_mmbtu = 1.e-3
 
 # print(plot_util.ETHREE_COL)
 
-
-index_names = ['Climate Zone', 'Home Type', 'Vintage', 'Home Fuel']
-ylim = [0, 100]
+index_names = ['Climate Zone', 'Vintage', 'Home Type', 'Home Fuel']
+ylim = [0, 80]
 
 input_directory = r'S:\E3 Projects\SCE Building Electrification\Building Simulations\building_simulation_inputs_8-14-18'
 output_directory = r'S:\E3 Projects\SCE Building Electrification\Building Simulations\Result Figures\Total Energy'
 
 cz_str = ['CZ03', 'CZ04', 'CZ06', 'CZ09', 'CZ10', 'CZ12']
-hy_str = ['SFH', ]
+hy_str = ['Single-Family Home', 'Low-Rise Multifamily', 'High-Rise Multifamily']
+hy_dict = {'Single-Family Home': 'SFH', 'Low-Rise Multifamily': 'LRMF', 'High-Rise Multifamily': 'HRMF'}
 
-vt_str = ['1400', '2100', '2700']
+vt_str = ['1978', '1990s', 'New Construction']
+vt_dict = {'Single-Family Home': {'1990s': '2100', '1978': '1400', 'New Construction': '2700'},
+           'Low-Rise Multifamily': {'1990s': 'MF90', '1978': 'MF70', 'New Construction': 'MFNC'},
+           'High-Rise Multifamily': {'1990s': '8', '1978': '9', 'New Construction': '7'}
+           }
+
 fuel_str = ['Mixed', 'All-Electric']
-fuel_dict = {'Mixed': '1gas',
-             'All-Electric': '2electric'}
+fuel_dict = {'Single-Family Home': {'Mixed': '1gas', 'All-Electric': '2electric'},
+             'Low-Rise Multifamily': {'Mixed': '1gas', 'All-Electric': '2electric'},
+             'High-Rise Multifamily': {'Mixed': 'G', 'All-Electric': 'E'}
+             }
+
 fuel_types = ['Electric (Mixed)', 'Gas (Mixed)', 'All-Electric']
-fuel_lookup = {'Electric (Mixed)': 'Total Electricity [kWh]',
-               'Gas (Mixed)': 'Total Gas [kBtu]',
-               'All-Electric': 'Total Electricity [kWh]'}
+fuel_lookup = {}
+fuel_lookup['Single-Family Home'] = {'Electric (Mixed)': 'Total Electricity [kWh]',
+                                     'Gas (Mixed)': 'Total Gas [kBtu]',
+                                     'All-Electric': 'Total Electricity [kWh]'}
+fuel_lookup['Low-Rise Multifamily'] = fuel_lookup['Single-Family Home']
+fuel_lookup['High-Rise Multifamily'] = {'Electric (Mixed)': 'Building Electricity [kWh]',
+                                        'Gas (Mixed)': 'Building Natural Gas [kBtu]',
+                                        'All-Electric': 'Building Electricity [kWh]'}
+
 scaling = {'Electric (Mixed)': kwh_to_mmbtu,
            'Gas (Mixed)': kbtu_to_mmbtu,
            'All-Electric': kwh_to_mmbtu}
 
-vt_dict = {
-    '1400': 'Pre-1978',
-    '2100': '1990s',
-    '2700': 'New'
-}
+house_scaling = {'Single-Family Home': 1, 'Low-Rise Multifamily': 1, 'High-Rise Multifamily': 1 / 32}
 
+'''
 ele_util_dict = {
     'CZ03': ['PG&E', ],
     'CZ04': ['PG&E', ],
@@ -55,7 +66,6 @@ ele_util_dict = {
     'CZ10': ['SCE', ],
 }
 
-'''
 util_trans = {
     'PG&E': 'PG&E-TOU',
     'SMUD': 'SMUD-TOU',
@@ -80,38 +90,43 @@ ann_gas_sav_df = pd.DataFrame()
 '''
 
 data = pd.DataFrame(
-    index=pd.MultiIndex.from_product([cz_str, hy_str, vt_str, [fuel_dict[x] for x in fuel_str]], names=index_names),
+    index=pd.MultiIndex.from_product([cz_str, vt_str, hy_str, fuel_str], names=index_names),
     columns=fuel_types)
 
 for idx in data.index.tolist():
     print(idx)
-    vt = idx[2]
-    hy = idx[1]
+    hy = idx[2]
+    vt = idx[1]
     cz = idx[0]
     fuel = idx[3]
-    sce_nm = vt + '_' + cz + '_' + fuel
-    print(sce_nm)
-    inputfile = os.path.join(input_directory, sce_nm + '.csv')
+    if hy == 'High-Rise Multifamily':
+        name = fuel_dict[hy][fuel] + vt_dict[hy][vt] + '_' + cz
+    else:
+        name = vt_dict[hy][vt] + '_' + cz + '_' + fuel_dict[hy][fuel]
+    print(name)
+    inputfile = os.path.join(input_directory, name + '.csv')
 
     df = pd.read_csv(inputfile, index_col=0)
 
     for fl in fuel_types:
-        if (fl == 'All-Electric' and fuel == fuel_dict['Mixed']) or (
-                fl == 'Electric (Mixed)' and fuel == fuel_dict['All-Electric']):
+        if (fl == 'All-Electric' and fuel == 'Mixed') or (
+                fl == 'Electric (Mixed)' and fuel == 'All-Electric'):
             data.loc[idx, fl] = 0.
         else:
-            data.loc[idx, fl] = df[fuel_lookup[fl]].sum() * scaling[fl]
+            data.loc[idx, fl] = df[fuel_lookup[hy][fl]].sum() * scaling[fl] * house_scaling[hy]
+
+data.to_csv(os.path.join(output_directory, 'Total Energy Consumption.csv'))
 
 group_size = 2
-num_series = len(vt_str) * len(fuel_str)
+num_series = len(hy_str) * len(fuel_str)
 # Plot!
-for cz, hy in itertools.product(cz_str, hy_str):
-    df = data.loc[cz, hy]
+for cz, vt in itertools.product(cz_str, vt_str):
+    df = data.loc[cz, vt]
     ax = df.plot.bar(stacked=True, rot=0, color=[plot_util.ETHREE_COL[x] for x in [1, 0, 1]], fontsize=fontsize)
     ax.set_xticks(range(0, len(df.index), 2))
-    ax.set_xticklabels([vt_dict[x] for x in df.index.levels[0].tolist()], fontsize=fontsize)
+    ax.set_xticklabels([hy_dict[x] for x in hy_str], fontsize=fontsize)
     ax.set_xlabel('')
-    ax.set_title(cz + ' ' + hy, fontsize=fontsize + 3)
+    ax.set_title(cz + ' ' + vt, fontsize=fontsize + 3)
     # set yaxis label
     ax.set_ylabel('Total Annual Energy (MMBtu)', fontsize=fontsize)
     ax.set_ylim(ylim)
@@ -125,4 +140,4 @@ for cz, hy in itertools.product(cz_str, hy_str):
 
     plt.legend(fontsize=fontsize)
 
-    plt.savefig(os.path.join(output_directory, cz + ' ' + hy + '.' + fmt), format=fmt)
+    plt.savefig(os.path.join(output_directory, cz + ' ' + vt + '.' + fmt), format=fmt)
